ae43d997195b310dabc61211cde8ede26cdf95d4
Nicolas Rodovalho <nicolasrlima@gmail.com>
2020-03-20 15:47:30 -0300

    custom controls estilizados

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-import { SlideNav } from './slide.js';
+import SlideNav from './slide.js';
 
 const slideNav = new SlideNav('.slide', '.slide-wrapper');
 slideNav.init(0);
 slideNav.addArrow('.prev', '.next');
-slideNav.addControl();
+slideNav.addControl('.custom-controls');
